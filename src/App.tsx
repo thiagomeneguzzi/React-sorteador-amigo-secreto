f1f0cae37a443dbafc49d629aa0d333b828acfd9
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-08-13 01:23:48 -0300

    fix - default select

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import Formulario from './components/Formulario';
+import { Configuracao } from './paginas/Configuracao';
+import Sorteio from './paginas/Sorteio';
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={<Formulario />}/>
+        <Route path='/' element={<Configuracao />}/>
+          <Route path='/sorteio' element={<Sorteio />}/>
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
--- src/paginas/Configuracao.tsx
+++ src/paginas/Configuracao.tsx
@@ -1,13 +1,17 @@
+import Card from "../components/Card"
 import Formulario from "../components/Formulario"
 import ListaParticipantes from "../components/ListaParticipantes"
 import Rodape from "../components/Rodape"
 
 export const Configuracao = () => {
   return (
-    <>
-      <Formulario />
-      <ListaParticipantes />
-      <Rodape />
-    </>
+    <Card>
+        <section>
+            <h2>Vamos começar!</h2>
+            <Formulario />
+            <ListaParticipantes />
+            <Rodape />
+        </section>
+    </Card>
   )
 }
